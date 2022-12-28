f696142976a0219c45bb1db6ed6be4bb0e4f030e
Zhenya <ndzhenya@mail.ru>
2022-12-29 00:13:59 +0300

    кол-во четных

--- Task_34/Program.cs
+++ Task_34/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void ArrayPrint(int[] a)
+{
+    Console.Write("[");
+    for (int i = 0; i < a.Length - 1; i++) 
+    Console.Write($"{a[i]}, ");
+    Console.WriteLine($"{a[a.Length - 1]}]");
+}
+int[] Array = new int[10];
+int count=0;
+for (int i = 0; i < Array.Length; i++) 
+Array[i] = new Random().Next(100, 1000);
+ArrayPrint(Array);
+Console.WriteLine("Найдем количество количество чётных чисел в массиве");
+for (int i=0; i<Array.Length; i++)
+{
+    if (Array[i] % 2==0) 
+    count +=1;
+    }
+ Console.WriteLine("Четных элементов:" + count);
--- Task_36/Program.cs
+++ Task_36/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int[5];
+int sum = 0;
+for (int i =0; i <array.Length; i++)
+{
+    array [i] = new Random().Next(1, 5);
+    Console.Write(array[i] + ", ");
+}
+for (int i =0; i <array.Length; i++)
+{
+    if (i % 2==1)
+    sum = sum + array[i];
+}
+Console.Write($"Сумма элементов, стоящих на нечетных позициях -> {sum}");
